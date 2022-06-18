3fb1dee32e320c3ea169a206ab1115ba8d65fdaa
Casey Frost <caseyfrost13@gmail.com>
2022-06-18 14:53:21 -0700

    created get tweets function

--- Prototype/get_tweets.py
+++ Prototype/get_tweets.py
@@ -1,46 +1,29 @@
 import tweepy
-from tweepy import OAuthHandler
 import pandas as pd
 from config import consumer_key, consumer_secret, access_token, access_token_secret
+from datetime import datetime, timedelta
+
+
+def get_tweets(query, amount):
+    tweet_list = list(tweepy.Cursor(api.search_tweets, q=query, count=100, lang='en').items(amount))
+    tweets = [[tweet.created_at, tweet.text, tweet.user._json['screen_name'], tweet.entities['urls']] for tweet in tweet_list]
+    return tweets
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-tweets = []
+yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+today = datetime.today().strftime('%Y-%m-%d')
 
-count = 1
+# query_1 = f'@SFBART Delay OR SFBARTalert OR #SFBART Delay filter:safe since:{yesterday} until:{today}'
+# query_2 = '@SFBART Delay OR SFBARTalert OR #SFBART Delay filter:safe'
+query = f'@SFBART OR #SFBART OR BART Delay since:{yesterday} until:{today}'
 
-"""Twitter will automatically sample the last 7 days of data. Depending on how many total tweets there are with the 
-specific hashtag, keyword, handle, or key phrase that you are looking for, you can set the date back further by 
-adding since= as one of the parameters. You can also manually add in the number of tweets you want to get back in the 
-items() section. """
+tweets = get_tweets(query=query, amount=300)
 
-for tweet in list(tweepy.Cursor(api.search_tweets, q="@SFBART Delay", count=450, lang='en').items(10)):
-
-    print(count)
-    count += 1
-
-    try:
-        data = [tweet.created_at, tweet.id, tweet.text, tweet.user._json['screen_name'], tweet.user._json['name'],
-                tweet.user._json['created_at'], tweet.entities['urls']]
-        data = tuple(data)
-        tweets.append(data)
-
-    except tweepy.TweepyException as e:
-        print(e)
-        continue
-
-    except StopIteration:
-        break
-
-df = pd.DataFrame(tweets, columns=['created_at', 'tweet_id', 'tweet_text', 'screen_name', 'name',
-                                   'account_creation_date', 'urls'])
+df = pd.DataFrame(tweets, columns=['created_at', 'tweet_text', 'screen_name', 'urls'])
 
 print(df.to_string())
-
-"""Add the path to the folder you want to save the CSV file in as well as what you want the CSV file to be named 
-inside the single quotations """
-# df.to_csv(path_or_buf='/Users/caseyfrost/Desktop/Springboard/GTFS_Capstone_Project/json_files/bart/tweets.nosync/test.csv',
-          # index=False)
