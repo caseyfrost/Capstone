1113238c40fd4423cd0d1dfb27fbc3df076d1aeb
Casey Frost <caseyfrost13@gmail.com>
2022-06-18 11:47:03 -0700

    Remove tokens, especially from remote.

--- Prototype/get_tweets.py
+++ Prototype/get_tweets.py
@@ -7,10 +7,7 @@ up correctly. And it's nice to get an uplifting message ;). """
 
 print("You got this!")
 
-access_token = '210205203-cGtRZgBuDwicEwKAVxicFtawr0WOBVCKBEY6mw2G'
-access_token_secret = '1dKrFmsxwxLI23SFfiEnlU6ZiyA9fxM7muYigP8KGMzft'
-consumer_key = 'gSC4fVWgNpmxEpMDi2cr8Aeqm'
-consumer_secret = 'T0mQlnu3yeD4FRKtMfm0Nvw1YnJR2Ra1oUAZeZYpxrBK5JK60L'
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -26,7 +23,7 @@ specific hashtag, keyword, handle, or key phrase that you are looking for, you c
 adding since= as one of the parameters. You can also manually add in the number of tweets you want to get back in the 
 items() section. """
 
-for tweet in list(tweepy.Cursor(api.search_tweets, q="@SFBART", count=450).items(10)):
+for tweet in list(tweepy.Cursor(api.search_tweets, q="@SFBART Delay", count=450, lang='en').items(10)):
 
     print(count)
     count += 1
@@ -47,7 +44,7 @@ for tweet in list(tweepy.Cursor(api.search_tweets, q="@SFBART", count=450).items
 df = pd.DataFrame(tweets, columns=['created_at', 'tweet_id', 'tweet_text', 'screen_name', 'name',
                                    'account_creation_date', 'urls'])
 
-print(df.head().to_string())
+print(df.to_string())
 
 """Add the path to the folder you want to save the CSV file in as well as what you want the CSV file to be named 
 inside the single quotations """
